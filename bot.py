1a7da747a98cdf1648484498887c520acce59583
Ivan <56882982+sfkl-i@users.noreply.github.com>
2020-06-21 18:21:18 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
+import settings
 
 logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
@@ -12,21 +13,21 @@ def greet_user(update, context):
 
 def talk_to_me(update, context):
     user_text = update.message.text
-    print(user_text)
-    update.message.reply_text(user_text)
+    print(user_text) 
+    update.message.reply_text(user_text) 
 
 
-def main():
-    mybot = Updater("1022195995:AAFz6dmMVWdHfVhHz5N773SAMDCgUgJgnKA", use_context=True)
+def main(): 
+    mybot = Updater(settings.API_KEY, use_context=True) 
 
     logging.info = 'Бот запускается.'
-
+ 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+ 
 
+    mybot.start_polling() 
+    mybot.idle() 
 
-    mybot.start_polling()
-    mybot.idle()
-
-main()
+main() 
